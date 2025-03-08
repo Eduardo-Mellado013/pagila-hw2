bcd85faeaebe0d30ee5deeb7f7cc82566da218ce
Eduardo-Mellado013 <eduardom71891@gmail.com>
2025-03-07 20:21:59 -0800

    Solved SQL Problems

--- sql/04.sql
+++ sql/04.sql
@@ -1,6 +1,14 @@
 /*
+ * 
  * Select the titles of all films that the customer with customer_id=1 has rented more than 1 time.
  *
  * HINT:
  * It's possible to solve this problem both with and without subqueries.
  */
+
+SELECT title FROM rental
+JOIN inventory USING (inventory_id)
+JOIN film USING (film_id)
+WHERE customer_id = 1 
+GROUP BY title
+HAVING count(customer_id) > 1;
--- sql/05b.sql
+++ sql/05b.sql
@@ -3,3 +3,9 @@
  * but instead of using the NOT IN operator, you are to use a LEFT JOIN.
  */
 
+SELECT last_name, first_name
+FROM actor
+LEFT JOIN customer
+USING (last_name, first_name)
+WHERE customer_id IS NULL
+ORDER BY last_name, first_name;
