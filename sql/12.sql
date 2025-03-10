35674511e8f7aac0248607117be5c193dba22ac1
Eduardo-Mellado013 <eduardom71891@gmail.com>
2025-03-09 21:10:03 -0700

    Solved SQL problems

--- sql/12.sql
+++ sql/12.sql
@@ -6,3 +6,8 @@
  * Create a select statement that lists the titles of all tables with the 'Trailers' special_feature.
  * Inner join the queries above.
  */
+
+SELECT title
+FROM film
+WHERE 'Behind the Scenes' IN (SELECT unnest(special_features)) AND 'Trailers' IN (SELECT unnest(special_features))
+ORDER BY title;
